be2332c9e10d3a48463075de121d71cf0a4cc21a
Patrick Schaefer <schaefpa@gruenau1.informatik.hu-berlin.de>
2022-08-26 17:25:59 +0200

    run-scripts

---
+++ run_sift1b.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# messi+sax+simd
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b.bin --in-memory --timeseries-size 128  --function-type 3 --SIMD --dataset-size 200000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b_queries.bin   --queries-size 100 --queue-number 12 --cpu-type 122 --is-norm --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
+
+# messi+sfa+best + ed
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b.bin --in-memory --timeseries-size 128  --function-type 4 --dataset-size 200000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b_queries.bin   --queries-size 100 --queue-number 12 --sample-size 1000000 --sample-type 3 --cpu-type 122 --is-norm --histogram-type 1 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000 --coeff-number 32
+
+# messi+sfa+best + ed
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b.bin --in-memory --timeseries-size 128  --function-type 4 --SIMD --dataset-size 200000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b_queries.bin   --queries-size 100 --queue-number 12 --sample-size 1000000 --sample-type 3 --cpu-type 122 --is-norm --histogram-type 1 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000 --coeff-number 32
+
+
+# messi+sfa+best + ew
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b.bin --in-memory --timeseries-size 128  --function-type 4 --dataset-size 200000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b_queries.bin   --queries-size 100 --queue-number 12 --sample-size 1000000 --sample-type 3 --cpu-type 122 --is-norm --histogram-type 2 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000 --coeff-number 32
+
+# messi+sfa+best + ew
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b.bin --in-memory --timeseries-size 128  --function-type 4 --SIMD --dataset-size 200000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /glusterfs/dfs-gfs-dist/brandjak-pub/sift1b_queries.bin   --queries-size 100 --queue-number 12 --sample-size 1000000 --sample-type 3 --cpu-type 122 --is-norm --histogram-type 2 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000 --coeff-number 32
+
+
