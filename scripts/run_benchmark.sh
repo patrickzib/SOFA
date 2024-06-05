a090760893c259206d38e6c69b05e59f771df221
Patrick Schaefer <patrick.schaefer@hu-berlin.de>
2024-06-05 12:18:32 +0200

    updates

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -4,90 +4,90 @@ set -e
 # ./run_cleanup.sh
 
 # SimSearchNet - ne
-./run_SimSearchNet_norm.sh 9 9
-./copy_files.sh SimSearchNet_ne 9
-./run_SimSearchNet_norm.sh 18 18
-./copy_files.sh SimSearchNet_ne 18
-./run_SimSearchNet_norm.sh 36 36
-./copy_files.sh SimSearchNet_ne 36
+#./run_SimSearchNet_norm.sh 9 9
+#./copy_files.sh SimSearchNet_ne 9
+#./run_SimSearchNet_norm.sh 18 18
+#./copy_files.sh SimSearchNet_ne 18
+#./run_SimSearchNet_norm.sh 36 36
+#./copy_files.sh SimSearchNet_ne 36
 
 # turingANNs - ne
-./run_turingANNs_norm.sh 9 9
-./copy_files.sh turingANNs_ne 9
-./run_turingANNs_norm.sh 18 18
-./copy_files.sh turingANNs_ne 18
-./run_turingANNs_norm.sh 36 36
-./copy_files.sh turingANNs_ne 36
+#./run_turingANNs_norm.sh 9 9
+#./copy_files.sh turingANNs_ne 9
+#./run_turingANNs_norm.sh 18 18
+#./copy_files.sh turingANNs_ne 18
+#./run_turingANNs_norm.sh 36 36
+#./copy_files.sh turingANNs_ne 36
 
 # BigANN - ne
-./run_bigann_norm.sh 9 9
-./copy_files.sh BIGANN_ne 9
-./run_bigann_norm.sh 18 18
-./copy_files.sh BIGANN_ne 18
+#./run_bigann_norm.sh 9 9
+#./copy_files.sh BIGANN_ne 9
+#./run_bigann_norm.sh 18 18
+#./copy_files.sh BIGANN_ne 18
 ./run_bigann_norm.sh 36 36
 ./copy_files.sh BIGANN_ne 36
 
 # spacev1B - ne
-./run_spaceV1B_norm.sh 9 9
-./copy_files.sh SPACEV1B_ne 9
-./run_spaceV1B_norm.sh 18 18
-./copy_files.sh SPACEV1B_ne 18
-./run_spaceV1B_norm.sh 36 36
-./copy_files.sh SPACEV1B_ne 36
+#./run_spaceV1B_norm.sh 9 9
+#./copy_files.sh SPACEV1B_ne 9
+#./run_spaceV1B_norm.sh 18 18
+#./copy_files.sh SPACEV1B_ne 18
+#./run_spaceV1B_norm.sh 36 36
+#./copy_files.sh SPACEV1B_ne 36
 
 # text-to-image - ne
-./run_text_to_image_norm.sh 9 9
-./copy_files.sh TEXTTOIMAGE_ne 9
-./run_text_to_image_norm.sh 18 18
-./copy_files.sh TEXTTOIMAGE_ne 18
-./run_text_to_image_norm.sh 36 36
-./copy_files.sh TEXTTOIMAGE_ne 36
+#./run_text_to_image_norm.sh 9 9
+#./copy_files.sh TEXTTOIMAGE_ne 9
+#./run_text_to_image_norm.sh 18 18
+#./copy_files.sh TEXTTOIMAGE_ne 18
+#./run_text_to_image_norm.sh 36 36
+#./copy_files.sh TEXTTOIMAGE_ne 36
 
 # SEISMIC
-./run_seismic.sh 9 9
-./copy_files.sh SEISMIC 9
-./run_seismic.sh 18 18
-./copy_files.sh SEISMIC 18
-./run_seismic.sh 36 36
-./copy_files.sh SEISMIC 36
+#./run_seismic.sh 9 9
+#./copy_files.sh SEISMIC 9
+#./run_seismic.sh 18 18
+#./copy_files.sh SEISMIC 18
+#./run_seismic.sh 36 36
+#./copy_files.sh SEISMIC 36
 
 # SALD
-./run_sald.sh 9 9
-./copy_files.sh SALD 9
-./run_sald.sh 18 18
-./copy_files.sh SALD 18
+#./run_sald.sh 9 9
+#./copy_files.sh SALD 9
+#./run_sald.sh 18 18
+#./copy_files.sh SALD 18
 ./run_sald.sh 36 36
 ./copy_files.sh SALD 36
 
 # SIFT1B
-./run_sift1b.sh 9 9
-./copy_files.sh SIFT1b 9
-./run_sift1b.sh 18 18
-./copy_files.sh SIFT1b 18
+#./run_sift1b.sh 9 9
+#./copy_files.sh SIFT1b 9
+#./run_sift1b.sh 18 18
+#./copy_files.sh SIFT1b 18
 ./run_sift1b.sh 36 36
 ./copy_files.sh SIFT1b 36
 
 # DEEP1B
-./run_deep1b.sh 9 9
-./copy_files.sh DEEP1b 9
-./run_deep1b.sh 18 18
-./copy_files.sh DEEP1b 18
+#./run_deep1b.sh 9 9
+#./copy_files.sh DEEP1b 9
+#./run_deep1b.sh 18 18
+#./copy_files.sh DEEP1b 18
 ./run_deep1b.sh 36 36
 ./copy_files.sh DEEP1b 36
 
 # SCEDC
-./run_scedc.sh 9 9
-./copy_files.sh SCEDC 9
-./run_scedc.sh 18 18
-./copy_files.sh SCEDC 18
+#./run_scedc.sh 9 9
+#./copy_files.sh SCEDC 9
+#./run_scedc.sh 18 18
+#./copy_files.sh SCEDC 18
 ./run_scedc.sh 36 36
 ./copy_files.sh SCEDC 36
 
 # ASTRO
-./run_astro.sh 9 9
-./copy_files.sh ASTRO 9
-./run_astro.sh 18 18
-./copy_files.sh ASTRO 18
+#./run_astro.sh 9 9
+#./copy_files.sh ASTRO 9
+#./run_astro.sh 18 18
+#./copy_files.sh ASTRO 18
 ./run_astro.sh 36 36
 ./copy_files.sh ASTRO 36
 
