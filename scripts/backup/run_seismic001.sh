46a630585ffeb25899e5ecb2115cea79edada6c4
Patrick Schaefer <patrick.schaefer@hu-berlin.de>
2024-06-05 09:18:24 +0200

    script upodate

--- scripts/backup/run_seismic001.sh
+++ scripts/backup/run_seismic001.sh
@@ -4,23 +4,23 @@ set -e
 
 
 # messi+sax+simd
-./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic_new.bin --in-memory --timeseries-size 256  --function-type 3 --SIMD --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin   --queries-size 100 --queue-number $2 --cpu-type $1 --is-norm --leaf-size 20000 --min-leaf-size 20000 --initial-lbl-size 20000
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic.bin --in-memory --timeseries-size 256  --function-type 3 --SIMD --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin   --queries-size 100 --queue-number $2 --cpu-type $1 --is-norm --leaf-size 20000 --min-leaf-size 20000 --initial-lbl-size 20000
 
 # messi+sfa+best + equi-width
-./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic_new.bin --in-memory --timeseries-size 256  --function-type 4 --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin --queries-size 100 --queue-number $2 --sample-size 10000000 --sample-type 3 --cpu-type $1 --is-norm --histogram-type 2 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic.bin --in-memory --timeseries-size 256  --function-type 4 --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin --queries-size 100 --queue-number $2 --sample-size 10000000 --sample-type 3 --cpu-type $1 --is-norm --histogram-type 2 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
 # --coeff-number 32
 
 # messi+sfa+best + equi-width
-./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic_new.bin --in-memory --timeseries-size 256  --function-type 4 --SIMD --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin --queries-size 100 --queue-number $2 --sample-size 10000000 --sample-type 3 --cpu-type $1 --is-norm --histogram-type 2 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic.bin --in-memory --timeseries-size 256  --function-type 4 --SIMD --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin --queries-size 100 --queue-number $2 --sample-size 10000000 --sample-type 3 --cpu-type $1 --is-norm --histogram-type 2 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
 # --coeff-number 32
 
 
 # messi+sfa+best
-./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic_new.bin --in-memory --timeseries-size 256  --function-type 4 --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin --queries-size 100 --queue-number $2 --sample-size 10000000 --sample-type 3 --cpu-type $1 --is-norm --histogram-type 1 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic.bin --in-memory --timeseries-size 256  --function-type 4 --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin --queries-size 100 --queue-number $2 --sample-size 10000000 --sample-type 3 --cpu-type $1 --is-norm --histogram-type 1 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
 # --coeff-number 32
 
 # messi+sfa+best
-./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic_new.bin --in-memory --timeseries-size 256  --function-type 4 --SIMD --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin --queries-size 100 --queue-number $2 --sample-size 10000000 --sample-type 3 --cpu-type $1 --is-norm --histogram-type 1 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
+./bin/MESSI --dataset /glusterfs/dfs-gfs-dist/brandjak-pub/seismic.bin --in-memory --timeseries-size 256  --function-type 4 --SIMD --dataset-size 100000000 --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries /vol/fob-wbib-vol2/wbi/schaefpa/messi/queries_botao/noiseSeismic001.bin --queries-size 100 --queue-number $2 --sample-size 10000000 --sample-type 3 --cpu-type $1 --is-norm --histogram-type 1 --leaf-size 10000 --min-leaf-size 10000 --initial-lbl-size 10000
 # --coeff-number 32
 
 
--- scripts/backup/run_workloads.sh
+++ scripts/backup/run_workloads.sh
@@ -2,60 +2,60 @@
 set -e
 
 # SEISMIC
-./run_seismic_new_workloads.sh 9 9 noiseSeismic001.bin
+./run_seismic_workloads.sh 9 9 noiseSeismic001.bin
 ./copy_files.sh SEISMIC001 9
-./run_seismic_new_workloads.sh 18 18 noiseSeismic001.bin
+./run_seismic_workloads.sh 18 18 noiseSeismic001.bin
 ./copy_files.sh SEISMIC001 18
-./run_seismic_new_workloads.sh 36 36 noiseSeismic001.bin
+./run_seismic_workloads.sh 36 36 noiseSeismic001.bin
 ./copy_files.sh SEISMIC001 36
 
-./run_seismic_new_workloads.sh 9 9 noiseSeismic002.bin
+./run_seismic_workloads.sh 9 9 noiseSeismic002.bin
 ./copy_files.sh SEISMIC002 9
-./run_seismic_new_workloads.sh 18 18 noiseSeismic002.bin
+./run_seismic_workloads.sh 18 18 noiseSeismic002.bin
 ./copy_files.sh SEISMIC002 18
-./run_seismic_new_workloads.sh 36 36 noiseSeismic002.bin
+./run_seismic_workloads.sh 36 36 noiseSeismic002.bin
 ./copy_files.sh SEISMIC002 36
 
-./run_seismic_new_workloads.sh 9 9 noiseSeismic005.bin
+./run_seismic_workloads.sh 9 9 noiseSeismic005.bin
 ./copy_files.sh SEISMIC005 9
-./run_seismic_new_workloads.sh 18 18 noiseSeismic005.bin
+./run_seismic_workloads.sh 18 18 noiseSeismic005.bin
 ./copy_files.sh SEISMIC005 18
-./run_seismic_new_workloads.sh 36 36 noiseSeismic005.bin
+./run_seismic_workloads.sh 36 36 noiseSeismic005.bin
 ./copy_files.sh SEISMIC005 36
 
-./run_seismic_new_workloads.sh 9 9 noiseSeismic01.bin
+./run_seismic_workloads.sh 9 9 noiseSeismic01.bin
 ./copy_files.sh SEISMIC01 9
-./run_seismic_new_workloads.sh 18 18 noiseSeismic01.bin
+./run_seismic_workloads.sh 18 18 noiseSeismic01.bin
 ./copy_files.sh SEISMIC01 18
-./run_seismic_new_workloads.sh 36 36 noiseSeismic01.bin
+./run_seismic_workloads.sh 36 36 noiseSeismic01.bin
 ./copy_files.sh SEISMIC01 36
 
 
 # SALD
-./run_sald_new_workloads.sh 9 9 noiseSALD001.bin
+./run_sald_workloads.sh 9 9 noiseSALD001.bin
 ./copy_files.sh SALD001 9
-./run_sald_new_workloads.sh 18 18 noiseSALD001.bin
+./run_sald_workloads.sh 18 18 noiseSALD001.bin
 ./copy_files.sh SALD001 18
-./run_sald_new_workloads.sh 36 36 noiseSALD001.bin
+./run_sald_workloads.sh 36 36 noiseSALD001.bin
 ./copy_files.sh SALD001 36
 
-./run_sald_new_workloads.sh 9 9 noiseSALD002.bin
+./run_sald_workloads.sh 9 9 noiseSALD002.bin
 ./copy_files.sh SALD002 9
-./run_sald_new_workloads.sh 18 18 noiseSALD002.bin
+./run_sald_workloads.sh 18 18 noiseSALD002.bin
 ./copy_files.sh SALD002 18
-./run_sald_new_workloads.sh 36 36 noiseSALD002.bin
+./run_sald_workloads.sh 36 36 noiseSALD002.bin
 ./copy_files.sh SALD002 36
 
-./run_sald_new_workloads.sh 9 9 noiseSALD005.bin
+./run_sald_workloads.sh 9 9 noiseSALD005.bin
 ./copy_files.sh SALD005 9
-./run_sald_new_workloads.sh 18 18 noiseSALD005.bin
+./run_sald_workloads.sh 18 18 noiseSALD005.bin
 ./copy_files.sh SALD005 18
-./run_sald_new_workloads.sh 36 36 noiseSALD005.bin
+./run_sald_workloads.sh 36 36 noiseSALD005.bin
 ./copy_files.sh SALD005 36
 
-./run_sald_new_workloads.sh 9 9 noiseSALD01.bin
+./run_sald_workloads.sh 9 9 noiseSALD01.bin
 ./copy_files.sh SALD01 9
-./run_sald_new_workloads.sh 18 18 noiseSALD01.bin
+./run_sald_workloads.sh 18 18 noiseSALD01.bin
 ./copy_files.sh SALD01 18
-./run_sald_new_workloads.sh 36 36 noiseSALD01.bin
+./run_sald_workloads.sh 36 36 noiseSALD01.bin
 ./copy_files.sh SALD01 36
