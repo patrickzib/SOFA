5feb536ee6b19471f29f1181d0f5105e7f8e4ec5
Patrick Schaefer <patrick.schaefer@hu-berlin.de>
2024-06-28 16:21:53 +0200

    updates

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -1,94 +1,139 @@
-s#!/bin/bash
+#!/bin/bash
 set -e
 
 # ./run_cleanup.sh
 
-# SimSearchNet - ne
-#./run_SimSearchNet_norm.sh 9 9
-#./copy_files.sh SimSearchNet_ne 9
-#./run_SimSearchNet_norm.sh 18 18
-#./copy_files.sh SimSearchNet_ne 18
-#./run_SimSearchNet_norm.sh 36 36
-#./copy_files.sh SimSearchNet_ne 36
+# Define a list of items
+items=(9 18 36)
 
-# turingANNs - ne
-#./run_turingANNs_norm.sh 9 9
-#./copy_files.sh turingANNs_ne 9
-#./run_turingANNs_norm.sh 18 18
-#./copy_files.sh turingANNs_ne 18
-#./run_turingANNs_norm.sh 36 36
-#./copy_files.sh turingANNs_ne 36
+# Iterate over each item in the list
+for threads in "${items[@]}"
+do
+    echo "The current $threads is: $$threads"
 
-# BigANN - ne
-#./run_bigann_norm.sh 9 9
-#./copy_files.sh BIGANN_ne 9
-#./run_bigann_norm.sh 18 18
-#./copy_files.sh BIGANN_ne 18
-./run_bigann_norm.sh 36 36
-./copy_files.sh BIGANN_ne 36
+    # SimSearchNet -
+    #./run_SimSearchNet_norm.sh $threads $threads
+    #./copy_files.sh SimSearchNet $threads
 
-# spacev1B - ne
-#./run_spaceV1B_norm.sh 9 9
-#./copy_files.sh SPACEV1B_ne 9
-#./run_spaceV1B_norm.sh 18 18
-#./copy_files.sh SPACEV1B_ne 18
-#./run_spaceV1B_norm.sh 36 36
-#./copy_files.sh SPACEV1B_ne 36
+    # turingANNs -
+    #./run_turingANNs_norm.sh 3$threads $threads
+    #./copy_files.sh turingANNs $threads
 
-# text-to-image - ne
-#./run_text_to_image_norm.sh 9 9
-#./copy_files.sh TEXTTOIMAGE_ne 9
-#./run_text_to_image_norm.sh 18 18
-#./copy_files.sh TEXTTOIMAGE_ne 18
-#./run_text_to_image_norm.sh 36 36
-#./copy_files.sh TEXTTOIMAGE_ne 36
+    # text-to-image -
+    #./run_text_to_image_norm.sh $threads $threads
+    #./copy_files.sh TEXTTOIMAGE $threads
 
-# SEISMIC
-#./run_seismic.sh 9 9
-#./copy_files.sh SEISMIC 9
-#./run_seismic.sh 18 18
-#./copy_files.sh SEISMIC 18
-#./run_seismic.sh 36 36
-#./copy_files.sh SEISMIC 36
+    # SEISMIC
+    #./run_seismic.sh $threads $threads
+    #./copy_files.sh SEISMIC $threads
 
-# SALD
-#./run_sald.sh 9 9
-#./copy_files.sh SALD 9
-#./run_sald.sh 18 18
-#./copy_files.sh SALD 18
-./run_sald.sh 36 36
-./copy_files.sh SALD 36
+    # spacev1B -
+    ##./run_spaceV1B_norm.sh $threads $threads
+    ##./copy_files.sh SPACEV1B $threads
 
-# SIFT1B
-#./run_sift1b.sh 9 9
-#./copy_files.sh SIFT1b 9
-#./run_sift1b.sh 18 18
-#./copy_files.sh SIFT1b 18
-./run_sift1b.sh 36 36
-./copy_files.sh SIFT1b 36
 
-# DEEP1B
-#./run_deep1b.sh 9 9
-#./copy_files.sh DEEP1b 9
-#./run_deep1b.sh 18 18
-#./copy_files.sh DEEP1b 18
-./run_deep1b.sh 36 36
-./copy_files.sh DEEP1b 36
+    # BigANN -
+    ./run_bigann_norm.sh $threads $threads
+    ./copy_files.sh BIGANN $threads
 
-# SCEDC
-#./run_scedc.sh 9 9
-#./copy_files.sh SCEDC 9
-#./run_scedc.sh 18 18
-#./copy_files.sh SCEDC 18
-./run_scedc.sh 36 36
-./copy_files.sh SCEDC 36
+    # SALD
+    ./run_sald.sh $threads $threads
+    ./copy_files.sh SALD $threads
 
-# ASTRO
-#./run_astro.sh 9 9
-#./copy_files.sh ASTRO 9
-#./run_astro.sh 18 18
-#./copy_files.sh ASTRO 18
-./run_astro.sh 36 36
-./copy_files.sh ASTRO 36
+    # SIFT1B
+    ./run_sift1b.sh $threads $threads
+    ./copy_files.sh SIFT1b $threads
+
+    # DEEP1B
+    ./run_deep1b.sh $threads $threads
+    ./copy_files.sh DEEP1b $threads
+
+    # SCEDC
+    ./run_scedc.sh $threads $threads
+    ./copy_files.sh SCEDC $threads
+
+    # ASTRO
+    ./run_astro.sh $threads $threads
+    ./copy_files.sh ASTRO $threads
+
+    # GEOFON
+    #./run_seisbench.sh $threads $threads "GEOFON.bin" "GEOFON_queries.bin" 38080393
+    #./copy_files.sh "GEOFON" $threads
+
+    # InstanceCounts
+    #./run_seisbench.sh $threads $threads "InstanceCounts.bin" "InstanceCounts_queries.bin" 100000000
+    #./copy_files.sh "InstanceCounts" $threads
+
+
+    # LFEStacksSanAndreasShelly2017
+    #./run_seisbench.sh $threads $threads "LFEStacksSanAndreasShelly2017.bin" "LFEStacksSanAndreasShelly2017_queries.bin" 215511
+    #./copy_files.sh "LFEStacksSanAndreasShelly2017" $threads
+
+    # LFEStacksCascadiaBostock2015
+    #./run_seisbench.sh $threads $threads "LFEStacksCascadiaBostock2015.bin" "LFEStacksCascadiaBostock2015_queries.bin" 169890
+    #./copy_files.sh "LFEStacksCascadiaBostock2015" $threads
+
+    # LFEStacksMexicoFrank2014
+    #./run_seisbench.sh $threads $threads "LFEStacksMexicoFrank2014.bin" "LFEStacksMexicoFrank2014_queries.bin" 1111683
+    #./copy_files.sh "LFEStacksMexicoFrank2014" $threads
+
+    # Ross2018GPD
+    #./run_seisbench.sh $threads $threads "Ross2018GPD.bin" "Ross2018GPD_queries.bin" 14320950
+    #./copy_files.sh "Ross2018GPD" $threads
+
+    # Ross2018JGRFM
+    #./run_seisbench.sh $threads $threads "Ross2018JGRFM.bin" "Ross2018JGRFM_queries.bin" 29082888
+    #./copy_files.sh "Ross2018JGRFM" $threads
+
+    # Ross2018JGRPick
+    #./run_seisbench.sh $threads $threads "Ross2018JGRPick.bin" "Ross2018JGRPick_queries.bin" 29082888
+    #./copy_files.sh "Ross2018JGRPick" $threads
+
+    # ETHC
+    ./run_seisbench.sh $threads $threads "ETHZ.bin" "ETHZ_queries.bin" 4999932
+    ./copy_files.sh "ETHC" $threads
+
+    # ISC_EHB_DepthPhases
+    ./run_seisbench.sh $threads $threads "ISC_EHB_DepthPhases.bin" "ISC_EHB_DepthPhases_queries.bin" 100000000
+    ./copy_files.sh "ISC_EHB_DepthPhases" $threads
+
+    # LenDB
+    ./run_seisbench.sh $threads $threads "LenDB.bin" "LenDB_queries.bin" 37345260
+    ./copy_files.sh "LenDB" $threads
+
+    # Iquique
+    ./run_seisbench.sh $threads $threads "Iquique.bin" "Iquique_queries.bin" 578853
+    ./copy_files.sh "Iquique" $threads
+
+    ## NEIC
+    ./run_seisbench.sh $threads $threads "NEIC.bin" "NEIC_queries.bin" 93473541
+    ./copy_files.sh "NEIC" $threads
+
+    # OBS
+    ./run_seisbench.sh $threads $threads "OBS.bin" "OBS_queries.bin" 15508794
+    ./copy_files.sh "OBS" $threads
+
+    # OBST2024
+    ./run_seisbench.sh $threads $threads "OBST2024.bin" "OBST2024_queries.bin" 4160286
+    ./copy_files.sh "OBST2024" $threads
+
+    # PNW
+    ./run_seisbench.sh $threads $threads "PNW.bin" "PNW_queries.bin" 31982766
+    ./copy_files.sh "PNW" $threads
+
+    # Meier2019JGR
+    ./run_seisbench.sh $threads $threads "Meier2019JGR.bin" "Meier2019JGR_queries.bin" 6361998
+    ./copy_files.sh "Meier2019JGR" $threads
+
+    # STEAD
+    ./run_seisbench.sh $threads $threads "STEAD.bin" "STEAD_queries.bin" 87323433
+    ./copy_files.sh "STEAD" $threads
+
+    # TXED
+    ./run_seisbench.sh $threads $threads "TXED.bin" "TXED_queries.bin" 35851641
+    ./copy_files.sh "TXED" $threads
+    
+
+done
 
 
--- scripts/run_deep1b.sh
+++ scripts/run_deep1b.sh
@@ -44,7 +44,7 @@ SAMPLE_SIZE=1000000
 QUERY_SIZE=100
 
 # messi+sax+simd
-$MESSI_BINARY --dataset $FILE_PATH --in-memory --timeseries-size $TS_SIZE  --function-type 3 --dataset-size $DATASET_SIZE --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries $QUERIES_PATH --queries-size $QUERY_SIZE --queue-number $2 --cpu-type $1 --is-norm --leaf-size 20000 --min-leaf-size 20000 --initial-lbl-size 20000 --SIMD
+$MESSI_BINARY --dataset $FILE_PATH --in-memory --timeseries-size $TS_SIZE  --function-type 3 --dataset-size $DATASET_SIZE --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries $QUERIES_PATH --queries-size $QUERY_SIZE --queue-number $2 --cpu-type $1 --leaf-size 20000 --min-leaf-size 20000 --initial-lbl-size 20000 --SIMD
 
 # messi+sfa+variance+equi-depth
 # $MESSI_BINARY --dataset $FILE_PATH --in-memory --timeseries-size $TS_SIZE  --function-type 4 --dataset-size $DATASET_SIZE --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries $QUERIES_PATH --queries-size $QUERY_SIZE --queue-number $2 --sample-size $SAMPLE_SIZE --sample-type 3 --cpu-type $1 --is-norm --histogram-type 1 --leaf-size 20000 --min-leaf-size 20000 --initial-lbl-size 20000 --coeff-number 0 --SIMD
