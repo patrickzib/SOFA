b25e4d72800071981b847fe0a76853883034b7f7
Patrick Schaefer <patrick.schaefer@hu-berlin.de>
2024-10-07 15:27:29 +0200

    sampling

---
+++ scripts/run_benchmark_sampling.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -e
+
+# ./run_cleanup.sh
+
+# Define a list of items
+threads=36
+items=(0.001 0.005 0.01 0.05 0.1)
+
+# Iterate over each item in the list
+for sampling_factor in "${items[@]}"
+do
+    echo "The current threads is: $threads"
+    echo "The current factor is: $sampling_factor"
+
+    # BigANN -
+    ./sampling_strategy/run_bigann_norm.sh $threads $threads $sampling_factor
+    ./copy_files.sh BIGANN $sampling_factor
+
+    # SALD
+    ./sampling_strategy/run_sald.sh $threads $threads $sampling_factor
+    ./copy_files.sh SALD $sampling_factor
+
+    # SIFT1B
+    ./sampling_strategy/run_sift1b.sh $threads $threads $sampling_factor
+    ./copy_files.sh SIFT1b $sampling_factor
+
+    # DEEP1B
+    ./sampling_strategy/run_deep1b.sh $threads $threads $sampling_factor
+    ./copy_files.sh DEEP1b $sampling_factor
+
+    # SCEDC
+    ./sampling_strategy/run_scedc.sh $threads $threads $sampling_factor
+    ./copy_files.sh SCEDC $sampling_factor
+
+    # ASTRO
+    ./sampling_strategy/run_astro.sh $threads $threads $sampling_factor
+    ./copy_files.sh ASTRO $sampling_factor
+
+    # ETHC
+    ./sampling_strategy/run_seisbench.sh $threads $threads "ETHZ.bin" "ETHZ_queries.bin" 4999932
+    ./copy_files.sh "ETHC" $sampling_factor
+
+    # ISC_EHB_DepthPhases
+    ./sampling_strategy/run_seisbench.sh $threads $threads "ISC_EHB_DepthPhases.bin" "ISC_EHB_DepthPhases_queries.bin" 100000000
+    ./copy_files.sh "ISC_EHB_DepthPhases" $sampling_factor
+
+    # LenDB
+    ./sampling_strategy/run_seisbench.sh $threads $threads "LenDB.bin" "LenDB_queries.bin" 37345260
+    ./copy_files.sh "LenDB" $sampling_factor
+
+    # Iquique
+    ./sampling_strategy/run_seisbench.sh $threads $threads "Iquique.bin" "Iquique_queries.bin" 578853
+    ./copy_files.sh "Iquique" $sampling_factor
+
+    ## NEIC
+    ./sampling_strategy/run_seisbench.sh $threads $threads "NEIC.bin" "NEIC_queries.bin" 93473541
+    ./copy_files.sh "NEIC" $sampling_factor
+
+    # OBS
+    ./sampling_strategy/run_seisbench.sh $threads $threads "OBS.bin" "OBS_queries.bin" 15508794
+    ./copy_files.sh "OBS" $sampling_factor
+
+    # OBST2024
+    ./sampling_strategy/run_seisbench.sh $threads $threads "OBST2024.bin" "OBST2024_queries.bin" 4160286
+    ./copy_files.sh "OBST2024" $sampling_factor
+
+    # PNW
+    ./sampling_strategy/run_seisbench.sh $threads $threads "PNW.bin" "PNW_queries.bin" 31982766
+    ./copy_files.sh "PNW" $sampling_factor
+
+    # Meier2019JGR
+    ./rsampling_strategy/un_seisbench.sh $threads $threads "Meier2019JGR.bin" "Meier2019JGR_queries.bin" 6361998
+    ./copy_files.sh "Meier2019JGR" $sampling_factor
+
+    # STEAD
+    ./sampling_strategy/run_seisbench.sh $threads $threads "STEAD.bin" "STEAD_queries.bin" 87323433
+    ./copy_files.sh "STEAD" $sampling_factor
+
+    # TXED
+    ./sampling_strategy/run_seisbench.sh $threads $threads "TXED.bin" "TXED_queries.bin" 35851641
+    ./copy_files.sh "TXED" $sampling_factor
+    
+
+done
+
+
---
+++ scripts/sampling_strategy/run_astro.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+
+if [ -z $1 ]; then
+    echo "Parameter cpu-type is empty"
+    exit 1
+fi
+if [ -z $2 ]; then
+    echo "Parameter queue-number is empty"
+    exit 1
+fi
+if [ -z "$3" ]
+   then
+     echo "Samling not set using default"
+     SAMPLING_FACTOR=0.01
+else
+     echo "Sampling factor" $3
+     SAMPLING_FACTOR=$3
+fi
+
+
+
+# SFA parameters:
+# --histogram-type
+#   equi-depth splitting: 1
+#   equi-width splitting: 2
+# -- function-type
+#   SFA: 4
+#   SAX: 3
+# -- sample-type
+#   first-n-values sampling: 1
+#   uniform sampling: 2
+#   random sampling: 3
+#	--sample-size
+#	  Set sample size for MCB
+
+MESSI_BINARY=../bin/MESSI
+FILE_PATH=/vol/tmp/schaefpa/messi_datasets/astro.bin
+QUERIES_PATH=/vol/tmp/schaefpa/messi_datasets/astro_queries.bin
+TS_SIZE=256
+
+COEFF_NUMBER=32
+DATASET_SIZE=100000000
+
+SAMPLE_SIZE=$(echo "$DATASET_SIZE * $SAMPLING_FACTOR" | bc)
+SAMPLE_SIZE=$(echo $SAMPLE_SIZE | awk '{printf "%.0f", $0}')
+QUERY_SIZE=100
+
+echo $SAMPLE_SIZE
+
+# messi+sfa+variance+simd+equi-depth
+$MESSI_BINARY --dataset $FILE_PATH --in-memory --timeseries-size $TS_SIZE  --function-type 4 --dataset-size $DATASET_SIZE --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries $QUERIES_PATH --queries-size $QUERY_SIZE --queue-number $2 --sample-size $SAMPLE_SIZE --sample-type 3 --cpu-type $1 --is-norm --histogram-type 1 --leaf-size 20000 --min-leaf-size 20000 --initial-lbl-size 20000 --coeff-number $COEFF_NUMBER --SIMD
+
+# messi+sfa+variance+simd+equi-width
+$MESSI_BINARY --dataset $FILE_PATH --in-memory --timeseries-size $TS_SIZE  --function-type 4 --dataset-size $DATASET_SIZE --flush-limit 300000 --read-block 20000 --sax-cardinality 8 --queries $QUERIES_PATH --queries-size $QUERY_SIZE --queue-number $2 --sample-size $SAMPLE_SIZE --sample-type 3 --cpu-type $1 --is-norm --histogram-type 2 --leaf-size 20000 --min-leaf-size 20000 --initial-lbl-size 20000 --coeff-number $COEFF_NUMBER  --SIMD
