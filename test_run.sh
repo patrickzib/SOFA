a8a0b7d94aa61d6d2bcb7c051c867e9729b5b82f
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2026-01-25 18:41:09 +0100

    tests

--- test_run.sh
+++ test_run.sh
@@ -3,10 +3,10 @@ QUERIES_PATH=data_queries/astro_queries.bin
 
 TS_SIZE=256
 COEFF_NUMBER=64
-QUERY_SIZE=10
+QUERY_SIZE=100
 
 DATASET_SIZE=10000
-SAMPLE_SIZE=1000
+SAMPLE_SIZE=10000
 
 CPU_TYPE=${82:-1}
 QUEUE_NUMBER=${8:-1}
@@ -20,8 +20,10 @@ QUEUE_NUMBER=${8:-1}
   --sample-size $SAMPLE_SIZE \
   --cpu-type $CPU_TYPE \
   --queue-number $QUEUE_NUMBER \
-  --function-type 3 \
+  --function-type 4 \
   --histogram-type 2 \
   --sfa-n-coefficients $COEFF_NUMBER \
-  --node-split-criterion 4 \
+  --leaf-size 10 \
+  --sax-cardinality 8 \
   --is-norm
+  # --node-split-criterion 4 \
