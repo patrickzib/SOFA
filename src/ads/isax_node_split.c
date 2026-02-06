15fb7530d658a9c409100288db7c0934af28afba
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2026-02-06 16:59:09 +0100

    update

--- src/ads/isax_node_split.c
+++ src/ads/isax_node_split.c
@@ -323,17 +323,21 @@ int maxbin_split_decision(isax_node_split_data *split_data,
 
 
 void split_node(isax_index *index, isax_node *node, int inmemory) {
+    int fallback_splitpoint = -1;
+
     if (node->mbb_sax_valid && node->mbb_sax_min != NULL && node->mbb_sax_max != NULL && node->parent != NULL) {
         int can_split = 0;
         for (int i = 0; i < index->settings->n_segments; ++i) {
             int current_cardinality = node->parent->split_data->split_mask[i];
+            if (current_cardinality >= index->settings->sax_bit_cardinality) {
+                continue;
+            }
             int next_bit_index = index->settings->sax_bit_cardinality - current_cardinality - 1;
-            if (next_bit_index >= 0) {
-                root_mask_type mask = index->settings->bit_masks[next_bit_index];
-                if ((node->mbb_sax_min[i] & mask) != (node->mbb_sax_max[i] & mask)) {
-                    can_split = 1;
-                    break;
-                }
+            root_mask_type mask = index->settings->bit_masks[next_bit_index];
+            if ((node->mbb_sax_min[i] & mask) != (node->mbb_sax_max[i] & mask)) {
+                can_split = 1;
+                fallback_splitpoint = i;
+                break;
             }
         }
         if (!can_split) {
@@ -415,9 +419,6 @@ void split_node(isax_index *index, isax_node *node, int inmemory) {
         append_disk_buffers(index, node, split_buffer, &split_buffer_index);
     }
 
-    // int fallback_splitpoint = -1;
-    //
-
     split_data->splitpoint = select_split_point(
         split_data,
         index->settings,
@@ -427,7 +428,8 @@ void split_node(isax_index *index, isax_node *node, int inmemory) {
     if (split_data->splitpoint < 0 ||
         split_data->split_mask[split_data->splitpoint] + 1 > index->settings->sax_bit_cardinality - 1) {
         fprintf(stderr, "error 2: cannot split in depth more than %d.\n", index->settings->sax_bit_cardinality);
-        return;
+        split_data->splitpoint = fallback_splitpoint;
+        // return;
         /*for (int s = 0; s < index->settings->n_segments; s++) {
             if (split_data->split_mask[s] + 1 <= index->settings->sax_bit_cardinality - 1) {
                 split_data->splitpoint = s;
