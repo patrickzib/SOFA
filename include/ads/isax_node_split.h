17eadb7149aadf6c21f973b9d57fe0b7bc03556f
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2026-02-08 23:32:17 +0100

    updates

--- include/ads/isax_node_split.h
+++ include/ads/isax_node_split.h
@@ -30,6 +30,6 @@ int maxbin_split_decision (isax_node_split_data * split_data,
                            isax_node_record * records_buffer,
                            int records_buffer_size);
 
-void split_node(isax_index *index, isax_node *node, int inmemory);
+int split_node(isax_index *index, isax_node *node, int inmemory);
 
 #endif
