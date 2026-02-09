777bfce81d6b143202495d1d5ef952fcd2f9265a
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2026-02-09 09:18:20 +0100

    updates

--- src/ads/isax_node_split.c
+++ src/ads/isax_node_split.c
@@ -388,7 +388,7 @@ int split_node(isax_index *index, isax_node *node, int inmemory) {
             }
         }
         if (!can_split) {
-            fprintf(stderr, "Not enough different symbols to split.\n");
+            // fprintf(stderr, "Not enough different symbols to split.\n");
             return 0;
         }
     }
