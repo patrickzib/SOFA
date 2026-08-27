688eecbea2f7401b559f9ad54127ec5e472cfaa4
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2026-08-27 17:50:53 +0200

    improve progress counter

--- src/ads/build_progress.c
+++ src/ads/build_progress.c
@@ -1,6 +1,13 @@
 #include "ads/build_progress.h"
 
 #include <stdio.h>
+#include <time.h>
+
+static double messi_progress_now_seconds(void) {
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    return (double) now.tv_sec + (double) now.tv_nsec / 1000000000.0;
+}
 
 void messi_build_progress_init(messi_build_progress *progress) {
     messi_build_progress_init_labeled(progress, "building index");
@@ -10,6 +17,7 @@ void messi_build_progress_init_labeled(messi_build_progress *progress, const cha
     pthread_mutex_init(&progress->lock, NULL);
     progress->last_percent = -1;
     progress->label = label;
+    progress->started_seconds = messi_progress_now_seconds();
     messi_build_progress_update(progress, 0.0);
 }
 
@@ -21,7 +29,12 @@ void messi_build_progress_update(messi_build_progress *progress, double percent)
 
     pthread_mutex_lock(&progress->lock);
     if (whole > progress->last_percent) {
-        fprintf(stderr, "\r>> %s: \x1b[32m%d%%\x1b[0m", progress->label, whole);
+        const double elapsed = messi_progress_now_seconds() - progress->started_seconds;
+        const double eta = percent > 0.0 ? elapsed * (100.0 - percent) / percent : -1.0;
+        fprintf(stderr, "\r>> %s: \x1b[32m%.2f%%\x1b[0m | elapsed %.0fs | ETA ",
+                progress->label, percent, elapsed);
+        if (eta >= 0.0) fprintf(stderr, "%.0fs", eta);
+        else fputs("--", stderr);
         fflush(stderr);
         progress->last_percent = whole;
     }
@@ -30,7 +43,9 @@ void messi_build_progress_update(messi_build_progress *progress, double percent)
 
 void messi_build_progress_finish(messi_build_progress *progress) {
     pthread_mutex_lock(&progress->lock);
-    fprintf(stderr, "\r>> %s: \x1b[32m100%%\x1b[0m\n", progress->label);
+    const double elapsed = messi_progress_now_seconds() - progress->started_seconds;
+    fprintf(stderr, "\r>> %s: \x1b[32m100.00%%\x1b[0m | elapsed %.0fs | ETA 0s\n",
+            progress->label, elapsed);
     fflush(stderr);
     progress->last_percent = 100;
     pthread_mutex_unlock(&progress->lock);
